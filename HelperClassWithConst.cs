4dbeded13a25179a60b7ba25192cddecd09eb731
slavafedchishin93@gmail.com <slavafedchishin93@gmail.com>
2021-12-05 18:28:28 +0300

    added class HelperClassWithConst;

---
+++ HelperClassWithConst.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fund_monitoring
+{
+    class HelperClassWithConst
+    {
+        public static readonly string pathToXls = "bins.xls";
+        public static readonly string sheetName = "Grid Results";
+        public static readonly string pathToJson = "cards.json";
+
+        public static readonly string connectionString = $@"
+                Provider=Microsoft.ACE.OLEDB.12.0;
+                Data Source={pathToXls};
+                Extended Properties=""Excel 12.0 Xml;HDR=YES""";
+    }
+}
