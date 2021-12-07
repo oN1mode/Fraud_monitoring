2ca0596095b284aa276a5e521a8109e7208e058d
slavafedchishin93@gmail.com <slavafedchishin93@gmail.com>
2021-12-07 18:58:56 +0300

    interface development;

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace Fund_monitoring
 {
@@ -20,12 +21,26 @@ namespace Fund_monitoring
     /// </summary>
     public partial class MainWindow : Window
     {
+        NumberCard numberCard;
         Cards cards;
         public MainWindow()
         {
             InitializeComponent();
             ParseXlsToJson.ParseAndSerializationXlsToJson();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
             cards = new Cards();
+            try
+            {
+                numberCard = new NumberCard(Convert.ToInt32(inputNumberCard.Text));
+            }
+            catch (FormatException fExc)
+            {
+                Debug.WriteLine($"{fExc.Message}, {fExc.GetType()}");
+                MessageBox.Show("Вы ввели некорректный номер карты!");
+            }
         }
     }
 }
--- Regulations.cs
+++ Regulations.cs
@@ -8,12 +8,15 @@ namespace Fund_monitoring
 {
     class Regulations
     {
+        #region Properties
         public int Bin { get; set; }
         public string Brand { get; set; }
         public string BinType { get; set; }
         public string BinLevel { get; set; }
         public string Country2Iso { get; set; }
         public string TypeOfTranscription { get; set; }
+        #endregion
+
         public Regulations()
         {
 
